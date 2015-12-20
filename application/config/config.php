b2e943aa8ba3323c87baf3cbb8b2ba1d04a8b18a
周瑞杰 <jacknj_zhou@163.com>
2015-12-20 20:51:20 +0800

    测试引入js css 方法路径
    测试引入外部资源方法

--- application/config/config.php
+++ application/config/config.php
@@ -130,7 +130,7 @@ $config['subclass_prefix'] = 'MY_';
 | Note: This will NOT disable or override the CodeIgniter-specific
 |	autoloading (application/config/autoload.php)
 */
-$config['composer_autoload'] = FALSE;
+$config['composer_autoload'] = TRUE;
 
 /*
 |--------------------------------------------------------------------------
@@ -207,7 +207,7 @@ $config['directory_trigger'] = 'd';
 | your log files will fill up very fast.
 |
 */
-$config['log_threshold'] = 0;
+$config['log_threshold'] = 1;
 
 /*
 |--------------------------------------------------------------------------
--- application/views/main.php
+++ application/views/main.php
@@ -1,6 +1,6 @@
 <html>
 <header>
-
+<script type="text/javascript " src=<?php=base_url()?>."static/public/js/bootstrap.min.js"></script>
 </header>
 <body>
 
